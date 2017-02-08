a1c087a827623f51fa1b28781ed5913eab161735
Scott Fay <scott.fay@invitae.com>
2017-02-08 00:30:37 -0800

    first commit

---
+++ flaskapp.py
@@ -0,0 +1,35 @@
+from flask import Flask, request
+import plotting
+import types
+import inspect
+
+app = Flask("corral")
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+def expose_fxns(app, module):
+    def _expose(fxn):
+        fxn_name = fxn.__name__
+        url_rule = '/{}/'.format(fxn_name)
+        for arg in inspect.getargspec(fxn).args:
+            url_rule = url_rule + "<" + arg + ">/"
+        url_rule = url_rule[:-1]
+        app.add_url_rule(rule=url_rule, endpoint=fxn_name, view_func=fxn)
+        return fxn
+    # iterates over the objects declared in a given module
+    for name in dir(module):
+        obj = getattr(module, name, None)
+        if isinstance(obj, types.FunctionType):
+            print name, obj
+            _expose(obj)
+
+
+expose_fxns(app, plotting)
+
+print app.url_map
+
+
+# build a page that has each of the plots in it
---
+++ plotting.py
@@ -0,0 +1,7 @@
+
+
+def endpoint1(aparam='default', param2='whatever'):
+    return "hello world! this worked! the parameter is set to: {}, {}".format(aparam, param2)
+
+
+
